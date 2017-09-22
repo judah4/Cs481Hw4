ed01329893a84ea463b00c576894533c748e638d
Judah Perez <tythin4@gmail.com>
2017-09-22 15:02:47 -0700

    project setup

---
+++ Cs481Hw4/Cs481Hw4/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cs481Hw4.Models;
+using Xamarin.Forms;
+
+namespace Cs481Hw4
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public ObservableCollection<RecipeModel> veggies { get; set; }
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            veggies = new ObservableCollection<RecipeModel> ();
+			veggies.Add (new RecipeModel{ Name="Tomato", Type="Fruit", Image="tomato.png"});
+			veggies.Add (new RecipeModel{ Name="Romaine Lettuce", Type="Vegetable", Image="lettuce.png"});
+			veggies.Add (new RecipeModel{ Name="Zucchini", Type="Vegetable", Image="zucchini.png"});
+            lstView.ItemsSource = veggies;
+        }
+    }
+}
