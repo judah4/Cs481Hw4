85693cd8842ff2c7c9356e5e4a69c9b05d440b6a
Judah Perez <tythin4@gmail.com>
2017-09-22 15:54:02 -0700

    octopus recipes

--- Cs481Hw4/Cs481Hw4/MainPage.xaml.cs
+++ Cs481Hw4/Cs481Hw4/MainPage.xaml.cs
@@ -24,10 +24,10 @@ namespace Cs481Hw4
 			veggies.Add (new RecipeModel{ Name="Octopus with Chorizo and Potatoes", Type="Octopus", Image="chorizooct.jpg", Website="http://www.foodandwine.com/recipes/octopus-chorizo-and-potatoes"});
 			veggies.Add (new RecipeModel{ Name="Octopus Turnovers with Spicy Creole Mayonnaise", Type="Octopus", Image="turnoveroct.jpg", Website="http://www.foodandwine.com/recipes/octopus-turnovers-with-spicy-creole-mayonnaise"});
             veggies.Add (new RecipeModel{ Name="Pan-Seared Octopus with Italian Vegetable Salad", Type="Octopus", Image="searedoct.jpg", Website="http://www.foodandwine.com/recipes/pan-seared-octopus-with-italian-vegetable-salad"});
-			veggies.Add (new RecipeModel{ Name="Grilled Octopus with Ancho Chile Sauce", Type="Octopus", Image="grilledoct.jpg", Website="http://www.foodandwine.com/recipes/grilled-octopus-ancho-chile-sauce"});
-			veggies.Add (new RecipeModel{ Name="Grilled Octopus with Ancho Chile Sauce", Type="Octopus", Image="grilledoct.jpg", Website="http://www.foodandwine.com/recipes/grilled-octopus-ancho-chile-sauce"});
-			veggies.Add (new RecipeModel{ Name="Grilled Octopus with Ancho Chile Sauce", Type="Octopus", Image="grilledoct.jpg", Website="http://www.foodandwine.com/recipes/grilled-octopus-ancho-chile-sauce"});
-
+			veggies.Add (new RecipeModel{ Name="Octopus with Black Bean-Pear Sauce", Type="Octopus", Image="beanoct.jpg", Website="http://www.foodandwine.com/recipes/octopus-with-black-bean-pear-sauce"});
+			veggies.Add (new RecipeModel{ Name="Octopus Salad with Potatoes and Green Beans", Type="Octopus", Image="saladoct.jpg", Website="http://www.foodandwine.com/recipes/octopus-salad-with-potatoes-and-green-beans"});
+			veggies.Add (new RecipeModel{ Name="Red Wine-Braised Baby Octopus with Black Olives", Type="Octopus", Image="wineoct.jpg", Website="http://www.foodandwine.com/recipes/red-wine-braised-baby-octopus-with-black-olives"});
+            veggies.Add (new RecipeModel{ Name="Spanish Braised Octopus in Paprika Sauce", Type="Octopus", Image="spanishoct.jpg", Website="https://www.thespruce.com/spanish-braised-octopus-in-paprika-sauce-1300677"});
             lstView.ItemsSource = veggies;
         }
 
@@ -52,7 +52,8 @@ namespace Cs481Hw4
 
         private void Handle_Refreshing(object sender, EventArgs e)
         {
-            
+            veggies.Add (new RecipeModel{ Name="Octopus and Seaweed Recipe", Type="Hot Dog", Image="hotdogoct.jpg", Website="https://www.tasteofhome.com/recipes/octopus-and-seaweed"});
+            lstView.EndRefresh();
         }
     }
 }
